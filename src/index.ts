b8bcc37f6c9e143c090cfdb48411e536bbe699dc
Axel Mathieu-Le Gall <aujoulator@hotmail.fr>
2025-06-05 21:29:09 +0200

    Refactor entry point and update main script references in package.json; remove obsolete index.ts file

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import { Server } from './server';
-
-dotenv.config();
-
-const port = parseInt(process.env.PORT || '3000', 10);
-const server = new Server(port);
-server.start();
